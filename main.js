fa20a19e726387ae656e62d737c37c05c1320a2d
djasinskis <danjasinski222@gmail.com>
2022-05-03 16:26:23 +0300

    teams

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+var countries = [
+    {country: "Lietuva", area: 65300, population: 2795680},
+    {country: "Latvija", area: 64589 , population: 1907675},
+    {country: "Estija", area: 45339, population: 1328439},
+    {country: "Kroatija", area: 56594, population: 3888529},
+    {country: "Sveicarija", area: 41285, population: 8570146},
+]
+
+for(var x of countries){
+    var average = (x.area * 1000000) / x.population
+    console.log(`Šalis: ${x.country}, joje gyvena: ${x.population} gyventojų. Valstybės plotas: ${x.area} km², plotas tenkantis vienam gyventojui: ${average.toFixed(2)} m² `)
+    console.log("=================")
+}
